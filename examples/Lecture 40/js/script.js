c45fa75bfb27e6dc64fdc74414bca5db6c793ab9
unknown <harshita77y@gmail.com>
2020-07-29 20:12:53 +0530

    Module 5

--- examples/Lecture 40/js/script.js
+++ examples/Lecture 40/js/script.js
@@ -1,13 +1,45 @@
-var x;
-console.log(x);
+//Event handling
+document.addEventListener("DOMContentLoaded",
+function (event) {
 
-if (x == undefined) {
-	console.log("x is undefined");
+	function sayHello (event) {
+		console.log(event);
+	this.textContent = "Said it!";
+	var name = document.getElementById("name").value;
+	var message = "<h2>Hello " + name + "!</h2>";
+
+	document
+	.getElementById("content")
+	.innerHTML = message;
+
+	if (name === "student") {
+		var title =
+		document
+			.querySelector("#title")
+			.textContent;
+		title+=" & Lovin'it";
+		document
+			.querySelector("#title")
+			.textContent = title;
+	}
 }
-x =5;
-if (x == undefined) {
-	console.log("x is undefined");
-}
-else {
-	console.log("x has been defined");
-}
+
+// Unobstructive event binding
+// document.querySelector("button")
+// 	.addEventListener("click", sayHello);
+
+document.querySelector("button")
+	.onclick = sayHello;
+
+document.querySelector("body")
+	.addEventListener("mousemove",
+		function (event) {
+			if (event.shiftKey === true) {
+			console.log("x: " + event.clientX);
+			console.log("y: " + event.clientY);
+		}
+	}
+	);
+
+	}
+);
